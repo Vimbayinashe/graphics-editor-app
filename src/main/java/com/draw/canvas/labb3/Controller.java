46767cf4a7805829a8a1ad948470fd72ea5f221d
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-10-22 20:52:11 +0200

    Adds canvas

--- src/main/java/com/draw/canvas/labb3/Controller.java
+++ src/main/java/com/draw/canvas/labb3/Controller.java
@@ -1,9 +1,11 @@
 package com.draw.canvas.labb3;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
 
 public class Controller {
+    public Canvas canvas;
     @FXML
     private Label welcomeText;
 
