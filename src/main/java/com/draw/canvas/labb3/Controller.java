4e515aecdc10acf201f204a04cc6c6530b5c36b8
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-10-22 21:13:13 +0200

    Adds styling to canvas & root

--- src/main/java/com/draw/canvas/labb3/Controller.java
+++ src/main/java/com/draw/canvas/labb3/Controller.java
@@ -3,9 +3,11 @@ package com.draw.canvas.labb3;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 
 public class Controller {
     public Canvas canvas;
+    public StackPane canvasParent;
     @FXML
     private Label welcomeText;
 
