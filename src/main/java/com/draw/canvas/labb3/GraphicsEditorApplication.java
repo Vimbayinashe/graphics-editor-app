aa392d3da8291745d00a326a2729300d68fbe881
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-11-05 20:18:15 +0100

    Renames .fxml file

--- src/main/java/com/draw/canvas/labb3/GraphicsEditorApplication.java
+++ src/main/java/com/draw/canvas/labb3/GraphicsEditorApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class GraphicsEditorApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GraphicsEditorApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GraphicsEditorApplication.class.getResource("graphics-editor-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 900);
         stage.setTitle("Graphics Editor");
         stage.setScene(scene);
